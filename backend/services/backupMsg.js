8043f7e24656f8f26567cb175c21518b405f3c40
bhuvanesproengineer <bhuvanes.proengineer@gmail.com>
2026-06-29 15:06:44 +0530

    Updated backupMsg.js

--- backend/services/backupMsg.js
+++ backend/services/backupMsg.js
@@ -21,8 +21,8 @@ export async function backupMsg(
             duration < 10
         ) {
 const shortSummary =
-    farmerSummary.length > 100
-        ? farmerSummary.substring(0, 100)
+    farmerSummary.length > 150
+        ? farmerSummary.substring(0, 150)
         : farmerSummary;
             console.log("SMS TEXT:");
 console.log(shortSummary);
