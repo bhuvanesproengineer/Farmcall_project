721a91ac116801d114e72fd804308b511c156417
bhuvanesproengineer <bhuvanes.proengineer@gmail.com>
2026-07-03 11:54:17 +0530

    Added the auto call features and to print the farmer in the console

--- backend/services/callAllFarmers.js
+++ backend/services/callAllFarmers.js
@@ -3,5 +3,7 @@ import {farmcall} from '../farmcall.js';
 export const callAllFarmers = async (farmers) => {
     for (const farmer of farmers) {
         await farmcall(farmer);
+        console.log(`Call made to farmer: ${farmer.farmer_name}`);g
+        
     }
 }
